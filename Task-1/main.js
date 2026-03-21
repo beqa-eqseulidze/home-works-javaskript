92788a9890b7c66f21409b04b911cc8d1a35454e
mestvirishvilierekle <mestvirishvilierekle.gmail.com>
2026-03-21 12:11:23 +0400

    funtcion-side

--- Task-1/main.js
+++ Task-1/main.js
@@ -1,3 +1,18 @@
-const span=document.getElementById("yourInfo")
-const userInformation=document.getElementById("inputInfo")
-user
+// const span=document.getElementById("yourInfo")
+// const userInformation=document.getElementById("inputInfo")
+// function info(){
+//     let infoText
+// }
+
+const encryptedmail=document.getElementById("yourInfo")
+const userEmail=document.getElementById("inputInfo");
+userEmail.addEventListener("change" , info);
+
+function info(){
+
+    let value=userEmail.value
+    if(value.includes("text")){
+        let index=value.indexOf("text")
+    }
+
+}
