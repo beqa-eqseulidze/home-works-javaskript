0cc19656e1fd52bf4c6237f17a6e1390ced79bf7
birtvelishvili-modus <birtvelishvili.modus@gmail.com>
2026-03-21 14:30:51 +0400

    add to js

--- task_2/main.js
+++ task_2/main.js
@@ -1,1 +1,10 @@
+    const input = document.getElementById("text");
+    const button = document.getElementById("btn");
+    const resultEl = document.getElementById("result");
+    const resultE2 = document.getElementById("result_2");
 
+    button.addEventListener("click", function () {
+      const result = input.value.trim().replaceAll(' ','-').repeat(4);
+      resultEl.textContent = result;
+    });
+    
