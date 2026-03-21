aeea159ae712d15da7d950a3fbdbe5597bbbf0bf
mestvirishvilierekle <mestvirishvilierekle.gmail.com>
2026-03-21 12:19:24 +0400

    function-2

--- Task-1/main.js
+++ Task-1/main.js
@@ -1,18 +1,16 @@
-// const span=document.getElementById("yourInfo")
-// const userInformation=document.getElementById("inputInfo")
-// function info(){
-//     let infoText
-// }
+const span=document.getElementById("yourInfo")
+const userInformation=document.getElementById("inputInfo")
+InputInfo.addEventListener("change" , info);
+function info(){
+    let infoText
+}
 
 const encryptedmail=document.getElementById("yourInfo")
 const userEmail=document.getElementById("inputInfo");
-userEmail.addEventListener("change" , info);
+
 
 function info(){
 
     let value=userEmail.value
-    if(value.includes("text")){
-        let index=value.indexOf("text")
-    }
-
+    
 }
