8ab8e3d04833c3eb55db8411eb620e27e7be5dbd
birtvelishvili-modus <birtvelishvili.modus@gmail.com>
2026-03-21 14:54:20 +0400

    modify  main.js

--- task_2/main.js
+++ task_2/main.js
@@ -1,7 +1,6 @@
     const input = document.getElementById("text");
     const button = document.getElementById("btn");
     const resultEl = document.getElementById("result");
-    const resultE2 = document.getElementById("result_2");
 
     button.addEventListener("click", function () {
       const result = input.value.replaceAll(' ','-').repeat(4);
