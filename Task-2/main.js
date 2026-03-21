c767ce6dda9684eb30fb50d5ea7fa442105849d3
mestvirishvilierekle <mestvirishvilierekle.gmail.com>
2026-03-21 14:51:16 +0400

    Complited-Task-2

--- Task-2/main.js
+++ Task-2/main.js
@@ -1,10 +1,17 @@
-const textInfo=document.getElementById("text")
-const numberInfo=+document.getElementById("number")
-const resultHere=document.getElementById("result")
-textInfo.addEventListener("change",info)
-number.addEventListener("change",info)
+const textInfo = document.getElementById("text");
+const numberInfo = document.getElementById("number");
+const resultHere = document.getElementById("result");
+textInfo.addEventListener("change", info);
+number.addEventListener("change", info);
 
-function info(){
-    let text=textInfo.value
-    resultHere.innerText=text
+function info() {
+  let text = textInfo.value;
+  let number = +numberInfo.value;
+  let res = "";
+  for (let i = 0; i < number; i++) {
+    res+= text + "-"
+}
+
+resultHere.innerText =res.slice(0,(res.length-2))
+
 }
