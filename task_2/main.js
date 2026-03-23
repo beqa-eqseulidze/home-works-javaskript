43fa0aecd6aca6e4daaac40e233244bab97e94d6
birtvelishvili-modus <birtvelishvili.modus@gmail.com>
2026-03-23 14:52:11 +0400

    modify to index.html and main.js

--- task_2/main.js
+++ task_2/main.js
@@ -1,9 +1,18 @@
-    const input = document.getElementById("text");
-    const button = document.getElementById("btn");
-    const resultEl = document.getElementById("result");
+// task_2/main.js
+const textInput = document.getElementById("text");
+const countInput = document.getElementById("number");
+const button = document.getElementById("btn");
+const resultEl = document.getElementById("result");
 
-    button.addEventListener("click", function () {
-      const result = input.value.replaceAll(' ','-').repeat(4);
-      resultEl.textContent = result;
-    });
-    
+button.addEventListener("click", function () {
+    const word = textInput.value;
+    const count = Number(countInput.value);
+
+    // abc, 3 --> abc-abc-abc
+    if (word && count > 0) {
+        let repeatedText = new Array(count).fill(word).join('-');
+        resultEl.textContent = repeatedText;
+    } else {
+        resultEl.textContent = "მიუთითეთ სიტყვა და რიცხვი!";
+    }
+});
