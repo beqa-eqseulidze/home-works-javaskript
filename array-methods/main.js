468a4dfb481e3342be89bfce65315ad17716dbda
beqa-eqseulidze <beqaeqseulidze6@gmail.com>
2026-03-28 14:24:02 +0400

    add reduce method

--- array-methods/main.js
+++ array-methods/main.js
@@ -21,45 +21,55 @@ const myNumber = [1, 2];
 // console.log('removeElement:', removeElement)
 // console.log('myNumber: ',myNumber);
 
-//find ეძებს ელემენტ მასივში
-const persons = [
-  { id: 1, firsName: "giorgi" },
-  { id: 2, firsName: "saba" },
-  { id: 3, firsName: "tornike" },
-  { id: 4, firsName: "bela" },
-  { id: 5, firsName: "tornike" },
-  { id: 6, firsName: "beqa" },
-];
-//find
-// const personId=persons.find((item,index,arr)=>item.id==6);
+// //find ეძებს ელემენტ მასივში
+// const persons = [
+//   { id: 1, firsName: "giorgi" },
+//   { id: 2, firsName: "saba" },
+//   { id: 3, firsName: "tornike" },
+//   { id: 4, firsName: "bela" },
+//   { id: 5, firsName: "tornike" },
+//   { id: 6, firsName: "beqa" },
+// ];
+// //find
+// // const personId=persons.find((item,index,arr)=>item.id==6);
 
-//foreach ეს მეთოდი ახალა მასივს არ აბრუნებს
-const a = persons.forEach((item, index, arr) => {
-  item.lastName = "default";
-});
+// //foreach ეს მეთოდი ახალა მასივს არ აბრუნებს
+// const a = persons.forEach((item, index, arr) => {
+//   item.lastName = "default";
+// });
 
-//map
-const copiedPersons = persons.map((item, index, arr) => {
-  return item;
-});
+// //map
+// const copiedPersons = persons.map((item, index, arr) => {
+//   return item;
+// });
 
-//findindex ეძებს სასურველ ელემენტს თავიდან და შეჩერდება სადც იპოვის
-const indexS = persons.findIndex((item, index, arr) => {
-  return item.firsName == "tornike";
-});
-console.log(indexS);
+// //findindex ეძებს სასურველ ელემენტს თავიდან და შეჩერდება სადც იპოვის
+// const indexS = persons.findIndex((item, index, arr) => {
+//   return item.firsName == "tornike";
+// });
+// console.log(indexS);
 
-//findindex ეძებს სასურველ ელემენტს მასივის ბოლოდან და შეჩერდება სადც იპოვის
-const indexE = persons.findLastIndex((item, index, arr) => {
-  return item.firsName == "tornike";
-});
+// //findindex ეძებს სასურველ ელემენტს მასივის ბოლოდან და შეჩერდება სადც იპოვის
+// const indexE = persons.findLastIndex((item, index, arr) => {
+//   return item.firsName == "tornike";
+// });
 
-//filter აბრუნებს სასურველი ელემენტების მასივს
-const filteredPersons = persons.filter((item, index, arr) => {
-  return item.id % 2 !== 0;
-});
+// //filter აბრუნებს სასურველი ელემენტების მასივს
+// const filteredPersons = persons.filter((item, index, arr) => {
+//   return item.id % 2 !== 0;
+// });
 
-console.log(filteredPersons);
+// console.log(filteredPersons);
 
-// console.log('copiedPersons: ',copiedPersons);
-// console.log('persons; ',persons)
+// // console.log('copiedPersons: ',copiedPersons);
+// // console.log('persons; ',persons)
+
+// არის მეთოდი reduce
+const arr = [1, 2, 3];
+const res = arr.reduce((prev, cur) => prev += cur ** 2, 0);
+
+//1 iteration: (0,1)=>{ return 0+=1**2 }()
+//2 iteration: (1,2)=>{ return 1+=2**2 }()
+//3 iteration: (5,3)=>{ return 5+=3**2 }()
+
+console.log(res);
