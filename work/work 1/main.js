aacb2a0fca801014d5cfbb3e208e974fa2df5fca
saba kecha <sabaforcollege@gmail.com>
2026-03-28 13:11:37 +0400

    square sum function

--- work/work 1/main.js
+++ work/work 1/main.js
@@ -1,6 +1,6 @@
 const numbers = [1, 2, 2]
 function square(numbers) {
-    const squared = numbers.map(num => num * num);
+    const squared = numbers.map((num) => num * num);
     let sum = 0;
 
     for (let i = 0; i < numbers.length; i++) {
