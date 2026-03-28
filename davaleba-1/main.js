a6ea912ea38328f5b8d543dee1070a64cfb1e84f
kakhaber17-bip <kaxaberlaptop@gmail.com>
2026-03-28 13:27:28 +0400

    square

---
+++ davaleba-1/main.js
@@ -0,0 +1,25 @@
+// Complete the square sum function so that it squares each number
+//  passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 because 
+
+
+
+
+function square(arr){
+  let sum = 0;
+
+  for(let i=0; i<arr.length; i++){
+    
+    sum+=arr[i] * arr[i];
+  }
+  return sum;
+}
+ 
+
+
+const result = square([1,2,2]);
+
+
+console.log("pasuxi", result); 
+
+
--- homework_kaxaber/main.js
+++
@@ -1,8 +0,0 @@
-console.log(array_Clone([1,2,4,0]));
-console.log(array_Clone([1,2,[4,0]]));
-
-
-const array_Clone = (arr) => {
-  return arr.slice(0);
-};
-
