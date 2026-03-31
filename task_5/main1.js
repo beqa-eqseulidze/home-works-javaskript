4adbebe308a3861bc41871dba0f1c03d511e4326
birtvelishvili-modus <birtvelishvili.modus@gmail.com>
2026-03-31 19:13:33 +0400

    add to main2.js

--- task_5/main1.js
+++ task_5/main1.js
@@ -1,14 +1,39 @@
-const items = [
-    {name: 'Apple', price:1.5},
-    {name: 'Laptop', price:1200},
-    {name: 'Notebook', price:5},
-    {name: 'Coffee', price:4},
-]
-function sum(items) {
-    return items.reduce((sum, current) => {
-        return sum + current.price
-    }, 0);
-}
-console.log(sum(items))
+// const items = [
+//     {name: 'Apple', price:1.5},
+//     {name: 'Laptop', price:1200},
+//     {name: 'Notebook', price:5},
+//     {name: 'Coffee', price:4},
+// ]
+// function sum(items) {
+//     return items.reduce((sum, current) => {
+//         return sum + current.price
+//     }, 0);
+// }
+// console.log(sum(items))
+
+// const votes = ['yes', 'no', 'yes', 'maybe', 'yes', 'no', 'obsent'];
+// function sum(search, array){
+//       let res=0;
+//       for(item of array) {
+//         if(search===item)res++
+//       }
+//     return res  
+// }
+//  const res=votes.reduce((prev,cur,index,arr) => {
+//      prev[cur] = sum(cur,arr);
+//     return prev
+// },{})
+// console.log(res)
+
+
+// const result = countVotes(votes)
+// function countVotes(votes) {
+//   return votes.reduce((acc, vote) => {
+//     acc[vote] = (acc[vote] || 0) + 1;
+//     return acc;
+//   }, {});
+// }
+
+
 
 
---
+++ task_5/main2.js
@@ -0,0 +1,3 @@
+const nested = [[1,2], [3,4], [5,6]];
+const newnested = nested.flat();
+console.log(newnested);
