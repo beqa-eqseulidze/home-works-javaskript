8b60d00f49fc4cb07649d3c0b15ad6e5c08a92b0
birtvelishvili-modus <birtvelishvili.modus@gmail.com>
2026-03-31 17:48:49 +0400

    Refactor squareSum function and add test cases
    Removed comments and added example test cases for squareSum function.

--- task_5/main.js
+++ task_5/main.js
@@ -1,14 +1,13 @@
 function squareSum(numbers) {
     return numbers.reduce((sum, current) => {
         if (Array.isArray(current)) {
-            // თუ ელემენტი მასივია, რეკურსიულად ვიძახებთ იგივე ფუნქციას
+           
             return sum + squareSum(current);
         }
-        // თუ ელემენტი რიცხვია, აგვყავს კვადრატში და ვამატებთ ჯამს
+     
         return sum + (current * current);
     }, 0);
 }
 
-// მაგალითების შემოწმება:
-console.log(squareSum([1, 2, 2])); // 1 + 4 + 4 = 9
-console.log(squareSum([1, 2, [3, 2]])); // 1 + 4 + (9 + 4) = 18
+console.log(squareSum([1, 2, 2])); 
+console.log(squareSum([1, 2, [3, 2]])); 
