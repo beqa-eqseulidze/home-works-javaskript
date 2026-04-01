f541098792083012c65ad13fbb43dd790275e088
kakhaber17 <kaxaberlaptop@gmail.com>
2026-04-01 13:19:01 +0400

    reduce-person

--- reduce_lesson/main.js
+++ reduce_lesson/main.js
@@ -1,5 +1,25 @@
+const people = [
+  { name: 'Alice', age: 25 },
+  { name: 'Bob', age: 30 },
+  { name: 'Charlie', age: 25 },
+  { name: 'Giorgi', age: 60 },
+  { name: 'David', age: 30 },
+  { name: 'John', age: 60 },
+  { name: 'Kaxa', age: 17 },
+  { name: 'Sam', age: 102 }
+];
 
-const arr=[1,2,3];
-arr.reduce((prev,cur)=>{
-    console.log('cur:', cur);
-},0)
+const group = people.reduce((res, person)=>{
+  const key = person.age;
+
+  if (!res[key]) {
+    res[key] = [];
+  }
+
+  res[key].push(person);
+
+  return res;
+
+},{});
+
+console.log(group);
