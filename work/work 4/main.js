297138b8f126dcef8ec814a7efa07009fe5d2e00
saba kecha <sabaforcollege@gmail.com>
2026-04-08 18:03:42 +0400

    adding .every/.some method

--- work/work 4/main.js
+++ work/work 4/main.js
@@ -4,7 +4,8 @@ const products = [
     { id: 3, name: "Shirt", price: 40, category: "Clothing", stock: 12 },
     { id: 4, name: "Phone", price: 800, category: "Electronics", stock: 10 },
     { id: 5, name: "Jeans", price: 60, category: "Clothing", stock: 3 },
-    { id: 6, name: "Keyboard", price: 100, category: "Electronics", stock: 8 }
+    { id: 6, name: "Keyboard", price: 100, category: "Electronics", stock: 8 },
+    { id: 1, name: "Laptop", price: 4000, category: "Electronics", stock: 5 }
 ]
 
 //პირველი დავალება
@@ -35,6 +36,7 @@ const sum = products.reduce((sum, product) => {
 console.log(sum);
 
 //მეხუთე დავალება
-const expensive = products.find(product => product.price > 3000);
+const res = products.every(product => product.price > 3000)
+// const res = products.some(product => product.price > 3000)
 
-console.log(!!expensive);
+console.log(res);
