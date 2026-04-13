a91618262f799d4fa2a7760e29cc79e3a72908bd
kakhaber17 <kaxaberlaptop@gmail.com>
2026-04-14 01:58:12 +0400

    product_table

---
+++ 14.04.26/main.js
@@ -0,0 +1,47 @@
+const products = [
+  { id: 1, name: "Laptop",     price: 2000, category: "Electronics", stock: 5 },
+  { id: 2, name: "Mouse",      price: 50,   category: "Electronics", stock: 0 },
+  { id: 3, name: "Shirt",      price: 40,   category: "Clothing",    stock: 12 },
+  { id: 4, name: "Phone",      price: 800,  category: "Electronics", stock: 10 },
+  { id: 5, name: "Jeans",      price: 60,   category: "Clothing",    stock: 3 },
+  { id: 6, name: "Keyboard",   price: 100,  category: "Electronics", stock: 8 }
+];
+
+
+function showProducts(curent){
+  let rows = "";
+
+  for (let i = 0; i < curent.length; i++){
+    let p = curent[i];
+    
+    rows += `
+      <tr>
+        <td>${p.id}</td>
+        <td>${p.name}</td>
+        <td>$${p.price}</td>
+        <td>${p.category}</td>
+        <td>${p.stock}</td>
+      </tr>
+    `;
+  }
+
+  document.getElementById("tableBody").innerHTML = rows;
+}
+
+
+showProducts(products);
+
+
+
+document.getElementById("filterButton").addEventListener("click", function() {
+  
+  let filtered = [];
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].stock > 0 && products[i].price > 50) {
+      filtered.push(products[i]);
+    }
+  }
+
+  showProducts(filtered);
+});
