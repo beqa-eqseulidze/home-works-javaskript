f44255e46d638e623a02a1d16eed1035482f59df
kakhaber17-bip <kaxaberlaptop@gmail.com>
2026-04-14 14:51:51 +0400

    productwork

--- 14.04.26/main.js
+++ 14.04.26/main.js
@@ -1,12 +1,11 @@
 const products = [
-  { id: 1, name: "Laptop",     price: 2000, category: "Electronics", stock: 5 },
-  { id: 2, name: "Mouse",      price: 50,   category: "Electronics", stock: 0 },
-  { id: 3, name: "Shirt",      price: 40,   category: "Clothing",    stock: 12 },
-  { id: 4, name: "Phone",      price: 800,  category: "Electronics", stock: 10 },
-  { id: 5, name: "Jeans",      price: 60,   category: "Clothing",    stock: 3 },
-  { id: 6, name: "Keyboard",   price: 100,  category: "Electronics", stock: 8 }
+  { id: 1, name: "Laptop", price: 2000, category: "Electronics", stock: 5 },
+  { id: 2, name: "Mouse", price: 50, category: "Electronics", stock: 0 },
+  { id: 3, name: "Shirt", price: 40, category: "Clothing", stock: 12 },
+  { id: 4, name: "Phone", price: 800, category: "Electronics", stock: 10 },
+  { id: 5, name: "Jeans", price: 60, category: "Clothing", stock: 3 },
+  { id: 6, name: "Keyboard", price: 100, category: "Electronics", stock: 8 }
 ];
-
 
 function showProducts(curent){
   let rows = "";
