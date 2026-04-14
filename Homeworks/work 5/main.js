eec8f1af855f6ec5132593af309284e6b2fc01a8
saba kecha <sabaforcollege@gmail.com>
2026-04-14 15:49:36 +0400

    fix filter table

--- Homeworks/work 5/main.js
+++ Homeworks/work 5/main.js
@@ -1,14 +1,14 @@
 const products = [
-    { id: 1, name: "Laptop", price: 2000, category: "Electronics", stock: 5 },
-    { id: 2, name: "Mouse", price: 50, category: "Electronics", stock: 0 },
-    { id: 3, name: "Shirt", price: 40, category: "Clothing", stock: 12 },
-    { id: 4, name: "Phone", price: 800, category: "Electronics", stock: 10 },
-    { id: 5, name: "Jeans", price: 60, category: "Clothing", stock: 3 },
-    { id: 6, name: "Keyboard", price: 100, category: "Electronics", stock: 8 }
+  { id: 1, name: "Laptop", price: 2000, category: "Electronics", stock: 5 },
+  { id: 2, name: "Mouse", price: 50, category: "Electronics", stock: 0 },
+  { id: 3, name: "Shirt", price: 40, category: "Clothing", stock: 12 },
+  { id: 4, name: "Phone", price: 800, category: "Electronics", stock: 10 },
+  { id: 5, name: "Jeans", price: 60, category: "Clothing", stock: 3 },
+  { id: 6, name: "Keyboard", price: 100, category: "Electronics", stock: 8 }
 ];
 
 const rows = products.map(product => {
-    return `
+  return `
     <tr>
       <td>${product.id}</td>
       <td>${product.name}</td>
@@ -38,8 +38,8 @@ const tableHTML = `
 document.getElementById("app").innerHTML = tableHTML;
 
 function renderTable(data) {
-    const rows = data.map(product => {
-        return `
+  const rows = data.map(product => {
+    return `
         <tr>
           <td>${product.id}</td>
           <td>${product.name}</td>
@@ -48,9 +48,9 @@ function renderTable(data) {
           <td>${product.stock}</td>
         </tr>
       `;
-    });
+  });
 
-    const tableHTML = `
+  const tableHTML = `
       <table border="1">
         <thead>
           <tr>
@@ -67,21 +67,21 @@ function renderTable(data) {
       </table>
     `;
 
-    document.getElementById("app").innerHTML = tableHTML;
+  document.getElementById("app").innerHTML = tableHTML;
 }
 
 document.getElementById("filterBtn").addEventListener("click", () => {
-    const categoryValue = document.getElementById("categoryInput").value.trim().toLowerCase();
-    const priceValue = document.getElementById("priceInput").value;
-    const stockValue = document.getElementById("stockInput").value;
+  const categoryValue = document.getElementById("categoryInput").value.trim().toLowerCase();
+  const priceValue = document.getElementById("priceInput").value;
+  const stockValue = document.getElementById("stockInput").value;
 
-    const filtered = products.filter(product => {
+  const filtered = products.filter(product => {
 
-        return (categoryValue === "" || product.category.toLowerCase() === categoryValue) &&
-            (priceValue === "" || product.price > Number(priceValue)) &&
-            (stockValue === "" || product.stock > Number(stockValue));
+    return (categoryValue === "" || product.category.toLowerCase() === categoryValue) &&
+      (priceValue === "" || product.price >= Number(priceValue)) &&
+      (stockValue === "" || product.stock >= Number(stockValue));
 
-    });
+  });
 
-    renderTable(filtered);
+  renderTable(filtered);
 });
