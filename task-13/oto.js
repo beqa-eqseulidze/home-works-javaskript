eeda7a7ed849a202e20d2b4e284ddbfccbb1977d
oyaulashvili-eng <oyaulashvili@gmail.com>
2026-04-24 18:03:03 +0400

    added task-13 changes
    claswork task-13

--- task-13/oto.js
+++ task-13/oto.js
@@ -1,3 +1,4 @@
+
   function updateClock() {
     let now = new Date();
 
@@ -14,3 +15,41 @@
   }
   setInterval(updateClock, 1000);
   updateClock();
+
+const users = [
+  {
+    id: 1,
+    name: "Aria",
+    profile: {
+      membership: { type: "Premium", active: true }
+    },
+    library: [
+      { title: "The Martian", rating: 4.5 },
+      { title: "Dune", rating: 4.8 }
+    ]
+  },
+  {
+    id: 2,
+    name: "Bob",
+    profile: null, // Bob has no profile!
+    library: [
+      { title: "Project Hail Mary", rating: 4.9 }
+    ]
+  },
+  {
+    id: 3,
+    name: "Cassie",
+    profile: {
+      membership: { type: "Basic", active: false }
+    },
+    // Cassie has no library property!
+  }
+];
+
+const titles = users
+  .map(user => user.library?.map(book => book.title))
+  .flat()
+  .slice(0, 3);
+
+console.log(titles);
+
