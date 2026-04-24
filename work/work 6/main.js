af8cb74b9be2762517cb1bd27fd37817723b557f
saba kecha <sabaforcollege@gmail.com>
2026-04-24 18:02:47 +0400

    Optional chaining

--- work/work 6/main.js
+++ work/work 6/main.js
@@ -1,4 +1,77 @@
-console.log(is_date("October 13,2014 11:13:00"));
-console.log(is_date(new Date(86400000)));
-console.log(is_date(new Date(99, 5, 24, 11, 33, 30, 0)));
-console.log(is_date([1, 2, 4, 0]));
+// const obj = {
+//     name: 'john',
+//     age: 21,
+// dog: { color: 'black', name: 'jeka' }
+// }
+
+// function dogName(obj) {
+//     if (obj.dog) {
+//         return obj.dog.name;
+//     }
+//     return "Dog not found";
+// }
+
+// console.log(dogName(obj));
+
+// function dogName(obj) {
+//     try {
+//         if (!obj.dog) {
+//             throw new Error("dog not found");
+//         }
+
+//         return obj.dog.name;
+
+//     } catch (error) {
+//         return "Dog not found";
+//     }
+//     finally {
+//         console.log("Function finished");
+//     }
+// }
+
+// console.log(dogName(obj));
+
+// function dogName(obj) {
+//     return obj.dog?.name || "Dog not found";
+// }
+
+// console.log(dogName(obj));
+
+const users = [
+    {
+        id: 1,
+        name: "Aria",
+        profile: {
+            membership: { type: "Premium", active: true }
+        },
+        library: [
+            { title: "The Martian", rating: 4.5 },
+            { title: "Dune", rating: 4.8 }
+        ]
+    },
+    {
+        id: 2,
+        name: "Bob",
+        profile: null, // Bob has no profile!
+        library: [
+            { title: "Project Hail Mary", rating: 4.9 }
+        ]
+    },
+    {
+        id: 3,
+        name: "Cassie",
+        profile: {
+            membership: { type: "Basic", active: false }
+        },
+        // Cassie has no library property!
+    }
+];
+function libraryProperty(users) {
+    const titles = users.flatMap(user =>
+        user.library?.map(book => book.title) ?? []
+    );
+
+    return titles.length ? titles : "title not found";
+}
+
+console.log(libraryProperty(users));
