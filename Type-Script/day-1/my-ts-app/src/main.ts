4943a9ffb67d2c427cc500a5bc84fe8a1e23133f
beqa-eqseulidze <beqaeqseulidze6@gmail.com>
2026-06-24 18:07:49 +0400

    add edit feture to ts todo project

--- Type-Script/day-1/my-ts-app/src/main.ts
+++ Type-Script/day-1/my-ts-app/src/main.ts
@@ -3,23 +3,33 @@ import type { ITodo, ITodoWithoutId } from "./models/todo";
 const form = document.querySelector("form");
 const table = document.querySelector("tbody");
 
-form?.addEventListener("submit", onSave);
+form?.addEventListener("submit", onCreate);
 
-const todos: ITodo[] = JSON.parse(localStorage.getItem('todos')??'[]');
+let todos: ITodo[] = JSON.parse(localStorage.getItem("todos") ?? "[]");
 renderToDo(todos);
+let todoForEdit: ITodo | null = null;
 
-function onSave(e: Event) {
+function onCreate(e: Event) {
   e.preventDefault();
   const formdata = new FormData(form!);
-  const todoWithoutId: ITodoWithoutId = Object.fromEntries(
+  const todoWithoutId = Object.fromEntries(
     formdata,
   ) as unknown as ITodoWithoutId;
-  const id = new Date().getTime();
-  const todo: ITodo = { ...todoWithoutId, id };
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
+
+  if (todoForEdit) {
+    todoForEdit.category = todoWithoutId.category;
+    todoForEdit.title = todoWithoutId.title;
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderToDo(todos);
+    todoForEdit = null;
+  } else {
+    const id = new Date().getTime();
+    const todo: ITodo = { ...todoWithoutId, id };
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderToDo(todos);
+  }
   form?.reset();
-  renderToDo(todos);
 }
 
 function renderToDo(data: ITodo[]): void {
@@ -29,7 +39,45 @@ function renderToDo(data: ITodo[]): void {
           <td>${item.id}</td>
           <td>${item.title}</td>
           <td>${item.category}</td>
+          <th>
+           <button class="edit" id="${item.id}" >შეცვლა</button>
+           <button class="delete" id="${item.id}" >წაშლა</button>
+          </th>
          </tr>
         `;
   });
+  const btnEdits = document.querySelectorAll(".edit");
+  const btnDeletes = document.querySelectorAll(".delete");
+
+  btnEdits.forEach((btn) => {
+    btn.addEventListener("click", onEdit);
+  });
+
+  btnDeletes.forEach((btn) => {
+    btn.addEventListener("click", onDelete);
+  });
+}
+
+function onEdit(e: Event): void {
+  const btn = e.target as HTMLElement;
+  if (btn && btn.id) {
+    let id = +btn.id;
+    todoForEdit = todos.find((el) => el.id == id) ?? null;
+    const inputs = form?.querySelectorAll("[name]") as unknown;
+    if(Array.isArray(inputs)){
+        inputs[0].value = todoForEdit?.title;
+        inputs[1].value = todoForEdit?.category;
+    }
+  }
+}
+
+function onDelete(e: Event): void {
+  const btn = e.target as HTMLElement;
+  if (btn && btn.id) {
+    let id = +btn.id;
+    let filteredTodos = todos.filter((el) => el.id != id);
+    todos = [...filteredTodos];
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderToDo(todos);
+  }
 }
