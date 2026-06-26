36473154d8e0bb9f301b056ab7ec87689602a711
kakhaber17-bip <kaxaberlaptop@gmail.com>
2026-06-26 17:53:53 +0400

    my-products-app

---
+++ products-app/src/main.ts
@@ -0,0 +1,38 @@
+const btnLogin = document.getElementById('login');
+const btnSignup = document.getElementById('signup');
+btnLogin?.addEventListener('click',login)
+btnSignup?.addEventListener('click',register)
+
+//login func
+ async function login() {  
+  const req = await fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+    },
+    body: JSON.stringify({
+      email: "kaxaber@mail.com",
+      password: "12345",
+    }),
+  });
+  const res=await req.json();
+  console.log(res)
+}
+
+//register func
+async function register() {  
+  const req = await fetch("http://localhost:3000/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: "kaxaber@mail.com",
+      password: "12345",
+    }),
+  });
+  const res = await req.json();
+  console.log("Registration Response:", res);
+}
+
+// registerTestUser();
