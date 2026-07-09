a543b77af950b5068e9bb6668dd750de06d3973b
kakhaber17-bip <kaxaberlaptop@gmail.com>
2026-07-09 15:17:47 +0400

    typescript-5

--- Typescript-5/src/main.ts
+++ Typescript-5/src/main.ts
@@ -1,4 +1,4 @@
-const MyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const MyNumbers : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 , 12];
 
 function processNumbers(nums: number[], filterFn: (n: number) => boolean,
   transformFn: (n: number) => number): number[] {
@@ -12,15 +12,15 @@ function processNumbers(nums: number[], filterFn: (n: number) => boolean,
 }
 
 
-const result1 = processNumbers(
+const result1: number[] = processNumbers(
   MyNumbers,
   (n) => n % 2 === 0,  //ვამოწმებ ლუწობას
   (n) => n * 2
 );
-console.log(result1);  // res - [4, 8, 12, 16, 20]
+console.log(result1);  // res - [4, 8, 12, 16, 20 , 24]
 
 
-const result2 = processNumbers(
+const result2: number[] = processNumbers(
   MyNumbers,
   (n) => n > 5,  //ვამოწმებ 5-ზე მეტობას
   (n) => n - 5 
