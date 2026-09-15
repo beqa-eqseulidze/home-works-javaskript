6c47bf7760a909cd19817d1f7a143dee8b598144
birtvelishvili-modus <birtvelishvili.modus@gmail.com>
2026-09-15 18:46:44 +0200

    add to new projects

--- TypeScript/DAY-1/my-ts-app/src/test.ts
+++ TypeScript/DAY-1/my-ts-app/src/test.ts
@@ -1,30 +1,30 @@
-// // interface IUser{
-// //     id:number,
-// //     name:string,
-// //     age?:number
-// // }
+// interface IUser{
+//     id:number,
+//     name:string,
+//     age?:number
+// }
 
-// // const user_1:IUser={
-// //     id:1,
-// //     name:'saba',
-// //     age:1
-// // }
+// const user_1:IUser={
+//     id:1,
+//     name:'saba',
+//     age:1
+// }
 
-// // const user_2:IUser={
-// //     id:1,
-// //     name:'saba'    
-// // }
+// const user_2:IUser={
+//     id:1,
+//     name:'saba'    
+// }
 
-// // let user_3:any={id:1};
-// // console.log(user_3.age);
+// let user_3:any={id:1};
+// console.log(user_3.age);
 
-// // let user_4:unknown='user 4'
-// // if( typeof user_4 =="string"){
-// //     console.log(user_4.length)
-// // }
+// let user_4:unknown='user 4'
+// if( typeof user_4 =="string"){
+//     console.log(user_4.length)
+// }
 
-// // const fn1:any=15;
-// // fn1();
+// const fn1:any=15;
+// fn1();
 
 // const fn2:unknown='hello'
 
@@ -32,4 +32,4 @@
 //     fn2();
 // }
 
-// // any
+// any
